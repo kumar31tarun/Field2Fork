1d5773bb46c74e01632136334cd2a0191805c6ac
Chetan Patil <patilchetan946@gmail.com>
2025-01-28 01:08:47 +0530

    User pojo updated

--- backend/src/main/java/com/field2fork/pojos/User.java
+++ backend/src/main/java/com/field2fork/pojos/User.java
@@ -6,8 +6,17 @@ import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.Lob;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 
 @Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 @Table(name = "users")
 public class User extends BaseEntity {
 
