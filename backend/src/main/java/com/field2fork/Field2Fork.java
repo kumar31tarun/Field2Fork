3ef2c6739ae177c92680f253e5e984a6007661a1
Chetan Patil <patilchetan946@gmail.com>
2025-01-27 22:25:10 +0530

    backend template updated

--- backend/src/main/java/com/field2fork/Field2Fork.java
+++ backend/src/main/java/com/field2fork/Field2Fork.java
@@ -1,4 +1,4 @@
-package com.blogs;
+package com.field2fork;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -8,23 +8,15 @@ import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class DemoApplication {
+public class Field2Fork {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(Field2Fork.class, args);
 	}
 		@Bean // equivalent to <bean id ..../> in xml file
 	public ModelMapper modelMapper() {
 		ModelMapper modelMapper = new ModelMapper();
-		modelMapper.getConfiguration()
-		.setMatchingStrategy(MatchingStrategies.STRICT) 
-		/* only MATCHING prop names between src n dest will be transferred , during the mapping
-		*/
-				.setPropertyCondition(Conditions.isNotNull());// only non null properties will be transferred from src
-																// --> dest , during the mapping
+		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT) .setPropertyCondition(Conditions.isNotNull());														
 		return modelMapper;
-
 	}
-
-
 }
