4b9b3abdea31c2eaada12398fc3c5ee92ad04f1d
Chetan Patil <patilchetan946@gmail.com>
2025-02-05 11:38:28 +0530

    Updated-template

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,44 @@
-import Header from "./components/Header";
-import Navbar from "./components/Navbar";
-import OfferCard from "./components/OfferCard";
-import WelcomeCard from "./components/WelcomeCard";
-import RenderOfferCard from "./components/RenderOfferCard";
-import ShopByCategory from "./components/ShopByCategory";
-import Footer from "./components/Footer";
-import FruitsAndVegetable from "./components/FruitsAndVegetables";
+// import Header from "./components/Header";
+// import Navbar from "./components/Navbar";
+// import OfferCard from "./components/OfferCard";
+// import WelcomeCard from "./components/WelcomeCard";
+// import RenderOfferCard from "./components/RenderOfferCard";
+// import ShopByCategory from "./components/ShopByCategory";
+// import Footer from "./components/Footer";
+// import FruitsAndVegetable from "./components/FruitsAndVegetables";
+
+// const App = () => {
+//   return (
+//     <div>
+//       <Header />
+//       <Navbar />
+//       <WelcomeCard />
+//       <RenderOfferCard />
+//       <ShopByCategory />
+//       <FruitsAndVegetable />
+//       <br />
+//       <Footer />
+//     </div>
+//   );
+// };
+
+// export default App;
+
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
-    <div>
-      <Header />
-      <Navbar />
-      <WelcomeCard />
-      <RenderOfferCard />
-      <ShopByCategory />
-      <FruitsAndVegetable />
-      <br />
-      <Footer />
-    </div>
+    <Router>
+      <Routes>
+        {" "}
+        {/* Use Routes instead of Switch */}
+        <Route path="/" element={<Home />} />{" "}
+        {/* Use element prop instead of component */}
+        {/* Add more routes as needed */}
+      </Routes>
+    </Router>
   );
 };
 
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,25 @@
+import Header from "../components/Header";
+import Navbar from "../components/Navbar";
+import OfferCard from "../components/OfferCard";
+import WelcomeCard from "../components/WelcomeCard";
+import RenderOfferCard from "../components/RenderOfferCard";
+import ShopByCategory from "../components/ShopByCategory";
+import Footer from "../components/Footer";
+import FruitsAndVegetable from "../components/FruitsAndVegetables";
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <Navbar />
+      <WelcomeCard />
+      <RenderOfferCard />
+      <ShopByCategory />
+      <FruitsAndVegetable />
+      <br />
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
