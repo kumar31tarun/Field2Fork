5b01d570e08614c4cc04c13853bc1822560bd484
Tarun Kumar <kumar31tarun@gmail.com>
2025-02-07 16:42:21 +0530

    Updated Frontend and backend

--- backend/src/main/java/com/field2fork/dao/UserDao.java
+++ backend/src/main/java/com/field2fork/dao/UserDao.java
@@ -7,7 +7,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-
+import com.field2fork.pojos.Role;
 import com.field2fork.pojos.User;
 
 public interface UserDao extends JpaRepository<User, Long> {
@@ -33,6 +33,14 @@ public interface UserDao extends JpaRepository<User, Long> {
 
 	@Query("SELECT u FROM User u WHERE u.id < :firstId AND u.role = 'SELLER' ORDER BY u.id DESC")
 	List<User> findPreviousSellers(Long firstId, Pageable pageRequest);
+	
+	 long countByActiveStatus(boolean activeStatus);
+
+	    @Query("SELECT COUNT(u) FROM User u WHERE u.role = :role")
+	    long countByRole(Role role);  // Use Role enum instead of String
+
+	    @Query("SELECT COUNT(u) FROM User u WHERE u.role = :role AND u.activeStatus = :activeStatus")
+	    long countByRoleAndActiveStatus(Role role, boolean activeStatus);
 
 }
 
